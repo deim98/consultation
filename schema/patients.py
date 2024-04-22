d506931bbfc2f2a1ebb403b11d5fe7cd108d8429
Dame Obaseki <136207695+deim98@users.noreply.github.com>
2024-04-23 00:36:33 +0100

    Update patients.py

--- schema/patients.py
+++ schema/patients.py
@@ -20,8 +20,8 @@ class PatientCreate(BaseModel):
     phone: str
 
 patients: list[Patient] = [
-    Patient(id= 0, firstname='Funke', lastname='Akindele', age= 25, sex='Female', height= 165, weight= 75, phone= '0705678998')
-    Patient(id= 1, firstname='Jeffery', lastname='Francis', age= 90, sex='Male', height= 195, weight= 90, phone= '0805874976')
-    Patient(id= 2, firstname='Aisha', lastname='Umar', age= 19, sex='Female', height= 150, weight= 55, phone= '0702673695')
+    Patient(id= 0, firstname='Funke', lastname='Akindele', age= 25, sex='Female', height= 165, weight= 75, phone= '0705678998'),
+    Patient(id= 1, firstname='Jeffery', lastname='Francis', age= 90, sex='Male', height= 195, weight= 90, phone= '0805874976'),
+    Patient(id= 2, firstname='Aisha', lastname='Umar', age= 19, sex='Female', height= 150, weight= 55, phone= '0702673695'),
     ]
 
