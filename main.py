3dd4c9e733a23cb4a068a54f0f7e0c58790bcfd5
Dame Obaseki <136207695+deim98@users.noreply.github.com>
2024-04-23 00:33:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
+from routers.doctors import doctor_router
+from routers.patients import patient_router
+from routers.appointment import appointment_router
+
 
 app = FastAPI()
 
-app.include_router(router=patient_router, prefix="/patients", tags=["patients"])
-app.include_router(router=doctor_router, prefix="/doctors", tags=["doctors"])
-app.include_router(router= appointment_router, prefix="/appointments", tags=["appointments"])
+app.include_router(patient_router, prefix="/patients", tags=["patients"])
+app.include_router(doctor_router, prefix="/doctors", tags=["doctors"])
+app.include_router(appointment_router, prefix="/appointments", tags=["appointments"])
 
-@app.get('/')
+@app.get('/home')
 def index():
     return "welcome to health is wealth"
